ca96b79bb103d529b97e7c7c269fd1f4533375f4
theoigen <oigenZ@icloud.com>
2019-07-09 13:38:25 +0300

    added navbar and useritem components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Eugene';
-
     return (
       <div className='App'>
-        <h1>Hello {name}</h1>
-        <h2>Goodbye</h2>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
